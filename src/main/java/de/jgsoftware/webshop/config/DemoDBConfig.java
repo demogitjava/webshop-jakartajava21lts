c06fadb300bc30f1ec9c7f80460c920601a691e7
hoscho <jgeist@gmx.net>
2023-09-26 00:21:12 +0200

    .

--- src/main/java/de/jgsoftware/webshop/config/DemoDBConfig.java
+++ src/main/java/de/jgsoftware/webshop/config/DemoDBConfig.java
@@ -1,27 +1,15 @@
 package de.jgsoftware.webshop.config;
 
-import java.sql.SQLException;
-import java.util.HashMap;
 
 //import javax.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
 
 import com.zaxxer.hikari.HikariConfig;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
@@ -31,12 +19,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 import javax.sql.DataSource;
 import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 //@EnableTransactionManagement
