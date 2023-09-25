45afa19730b985e09f664ac952148865a5520b0d
hoscho <jgeist@gmx.net>
2023-09-25 21:06:56 +0200

    .

---
+++ src/main/java/de/jgsoftware/webshop/demowebshop/DemoWebshop.java
@@ -0,0 +1,14 @@
+package de.jgsoftware.webshop.demowebshop;
+
+/**
+ *
+ * @author hoscho
+ */
+
+@SpringBootApplication
+public class DemoWebshop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
