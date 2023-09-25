e2a5bc271b3a47520cd998e701b6a4b5cbc80e57
hoscho <jgeist@gmx.net>
2023-09-25 21:39:10 +0200

    edit DemoWebshop main class

--- src/main/java/de/jgsoftware/webshop/DemoWebshop.java
+++ src/main/java/de/jgsoftware/webshop/DemoWebshop.java
@@ -1,6 +1,10 @@
 package de.jgsoftware.webshop;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 /**
@@ -8,10 +12,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author hoscho
  */
 
+
 @SpringBootApplication
+//@EnableJpaRepositories(basePackages = "de.jgsoftware.landingpage.dao.interfaces.*") 
+@EnableJpaRepositories
 public class DemoWebshop {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+     
+       SpringApplicationBuilder builder = new SpringApplicationBuilder(DemoWebshop.class);
+		builder
+				// none
+				// reactive
+				// servlet
+				.web(WebApplicationType.SERVLET).headless(true).run();
+
     }
 }
