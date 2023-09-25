10618ed238b2a7fc1dc7c4b7d64634debda828cd
hoscho <jgeist@gmx.net>
2023-09-25 23:35:19 +0200

    add daoindex with interface

---
+++ src/main/java/de/jgsoftware/webshop/dao/daoindex.java
@@ -0,0 +1,15 @@
+package de.jgsoftware.webshop.dao;
+
+import org.springframework.stereotype.Repository;
+import de.jgsoftware.webshop.dao.interfaces.i_daoindex;
+/**
+ *
+ * @author hoscho
+ */
+
+@Repository
+public class daoindex implements i_daoindex
+{
+    
+    
+}
---
+++ src/main/java/de/jgsoftware/webshop/dao/interfaces/i_daoindex.java
@@ -0,0 +1,9 @@
+package de.jgsoftware.webshop.dao.interfaces;
+
+/**
+ *
+ * @author hoscho
+ */
+public interface i_daoindex {
+    
+}
