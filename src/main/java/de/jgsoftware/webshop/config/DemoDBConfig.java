a768dc2f53d98c628df1d49a5eeee31544c0e880
hoscho <jgeist@gmx.net>
2023-09-27 21:05:00 +0200

    edit DemoDbConfig

--- src/main/java/de/jgsoftware/webshop/config/DemoDBConfig.java
+++ src/main/java/de/jgsoftware/webshop/config/DemoDBConfig.java
@@ -1,13 +1,9 @@
 package de.jgsoftware.webshop.config;
 
 
-import java.sql.SQLException;
 import java.util.HashMap;
-
 import jakarta.persistence.EntityManagerFactory;
-
 import javax.sql.DataSource;
-
 import com.zaxxer.hikari.HikariConfig;
 
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -22,22 +18,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
-import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "de.jgsoftware.webshop.dao.interfaces.demodb.*",
