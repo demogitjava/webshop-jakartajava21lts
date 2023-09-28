738d2607a5f7064b8cfae5fdb111415b694b44f2
hoscho <jgeist@gmx.net>
2023-09-28 22:23:59 +0200

    add SpringSecurityConfiguration

---
+++ src/main/java/de/jgsoftware/webshop/config/SpringSecurityConfiguration.java
@@ -0,0 +1,52 @@
+package de.jgsoftware.webshop.config;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+/**
+ *
+ * @author hoscho
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfiguration extends WebMvcConfigurerAdapter {
+
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder authBuilder) throws Exception {
+		authBuilder.jdbcAuthentication().dataSource(dataSource).passwordEncoder(new BCryptPasswordEncoder())
+				.authoritiesByUsernameQuery("select username, role from users where username=?")
+				.usersByUsernameQuery("select username, password, enabled from users where username=?");
+
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	protected void configure(HttpSecurity http) throws Exception {
+
+      
+	}
+
+}
