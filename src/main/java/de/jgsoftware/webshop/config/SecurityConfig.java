09ee447e87662ddf5e307fa3037ee539d2f87145
hoscho <jgeist@gmx.net>
2023-10-07 19:47:56 +0200

    edit security config

--- src/main/java/de/jgsoftware/webshop/config/SecurityConfig.java
+++ src/main/java/de/jgsoftware/webshop/config/SecurityConfig.java
@@ -1,10 +1,16 @@
 package de.jgsoftware.webshop.config;
 
-import org.springframework.context.annotation.Bean;
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,5 +18,18 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig extends WebSecurityConfiguration 
 {
 
- 
+	
+	    
+
+	    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	        UserDetails user = User.withDefaultPasswordEncoder()
+	            .username("root	")
+	            .password("jj78mvpr52k1")
+	            .roles("USER")
+	            .build();
+	        auth.jdbcAuthentication()
+	            .withDefaultSchema()
+	            //.dataSource(dataSource())
+	            .withUser(user);
+	    }
 }
