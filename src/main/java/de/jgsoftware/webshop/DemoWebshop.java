9f509095bd0bd38b9bb4c6ff6707a1e23d9ff62b
hoscho <jgeist@gmx.net>
2023-10-07 10:03:45 +0200

    edit main method to start application with servlet
    time zone is set to GMT Europe/Berlin

--- src/main/java/de/jgsoftware/webshop/DemoWebshop.java
+++ src/main/java/de/jgsoftware/webshop/DemoWebshop.java
@@ -3,12 +3,17 @@ package de.jgsoftware.webshop;
 import de.jgsoftware.webshop.config.Derbydb_Hiberante;
 import de.jgsoftware.webshop.config.Mawi_Hiberante;
 import de.jgsoftware.webshop.config.Shop_Hiberante;
-import org.springframework.boot.SpringApplication;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-
+import java.sql.Timestamp;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 
 /**
@@ -20,7 +25,11 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 @SpringBootApplication
 @EnableAutoConfiguration(
 exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-public class DemoWebshop {
+public class DemoWebshop 
+{
+	 public static String st_timezones = "Europe/Berlin";
+	 public static String operationsytem = null;
+	
 
     public static void main(String[] args) 
     {
@@ -41,7 +50,43 @@ public class DemoWebshop {
         shopdb.openSession();
         
         
-        SpringApplication.run(DemoWebshop.class, args);
+        //SpringApplication.run(DemoWebshop.class, args);
+        
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.of(st_timezones));
+        //ZonedDateTime now = ZonedDateTime.now();
+
+        // set up to GMT
+        now.withZoneSameInstant( ZoneId.of("GMT") )
+                .format( DateTimeFormatter.ISO_OFFSET_DATE_TIME );
+        // 1. ZonedDateTime to TimeStamp
+        Timestamp timestamp = Timestamp.valueOf(now.toLocalDateTime());
+
+        // 2. ZonedDateTime to TimeStamp , no different
+        Timestamp timestamp2 = Timestamp.from(now.toInstant());
+
+        System.out.println(now);
+        System.out.println(timestamp);
+        System.out.println(timestamp2);
+
+
+        String operationsytem = System.getProperty("os.name").toLowerCase();
+        if (operationsytem.contains("win")){
+            //Betriebssystem = Windows
+            operationsytem = "Windows";
+
+        }
+        else if (operationsytem.contains("osx")){
+            //Betriebssystem = OSX von Apple
+            operationsytem = "OSX Apple";
+        }
+        else if (operationsytem.contains("nix") || operationsytem.contains("aix") || operationsytem.contains("nux")){
+            //Betriebssystem = Unix bzw. Linux basiert
+            operationsytem = "unix-linux";
+        }
+        System.out.print(operationsytem + "\n");
+
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(DemoWebshop.class);
+        builder.web(WebApplicationType.SERVLET).headless(true).run();
 
     }
 }
--- src/main/java/de/jgsoftware/webshop/config/SecurityConfig.java
+++ src/main/java/de/jgsoftware/webshop/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests()
-                .requestMatchers("/blog/**").permitAll()
+                .requestMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
