f90333ba6d54cf3b463d3fcd8ae8bd5d334ce41b
hoscho <jgeist@gmx.net>
2023-10-16 07:11:40 +0200

    edit indexcontroller

--- src/main/java/de/jgsoftware/webshop/controller/IndexController.java
+++ src/main/java/de/jgsoftware/webshop/controller/IndexController.java
@@ -35,10 +35,15 @@ public class IndexController implements i_indexcontroller {
     	
         mv = new ModelAndView("index");
         //Long usercountdemo = interfaceindexservice.getuseragentcount();
+        
+        /*
+         *  database demodb
+         *  usercount 
+         */
         mv.addObject("Usercount", interfaceindexservice.getuseragentcount());
        
        
-        return null;
+        return mv;
     }
     
 }
