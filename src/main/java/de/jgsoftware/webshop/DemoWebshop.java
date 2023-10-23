45a99002b2031913ba285f61837f4bed231d5187
hoscho <jgeist@gmx.net>
2023-10-23 12:22:05 +0200

    edit DemoWebshop

--- src/main/java/de/jgsoftware/webshop/DemoWebshop.java
+++ src/main/java/de/jgsoftware/webshop/DemoWebshop.java
@@ -8,15 +8,10 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-
 import java.sql.Timestamp;
 import org.springframework.boot.SpringApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 
 
 
