993396d71c388c7c6f7faf3207cb7fb39ce155a9
hoscho <jgeist@gmx.net>
2023-10-23 12:22:29 +0200

    edit IndexController

--- src/main/java/de/jgsoftware/webshop/controller/IndexController.java
+++ src/main/java/de/jgsoftware/webshop/controller/IndexController.java
@@ -2,7 +2,6 @@ package de.jgsoftware.webshop.controller;
 
 import de.jgsoftware.webshop.controller.interfaces.i_indexcontroller;
 
-import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
