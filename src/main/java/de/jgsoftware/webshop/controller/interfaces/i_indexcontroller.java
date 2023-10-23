c3e2047b4ae35114c996f34edd2ffbdb299785e7
hoscho <jgeist@gmx.net>
2023-10-23 12:20:37 +0200

    edit i_indexcontroller

--- src/main/java/de/jgsoftware/webshop/controller/interfaces/i_indexcontroller.java
+++ src/main/java/de/jgsoftware/webshop/controller/interfaces/i_indexcontroller.java
@@ -3,8 +3,6 @@ package de.jgsoftware.webshop.controller.interfaces;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 /**
  *
