ffbc752049755dadf8c50a3b2cb40cd1aa27708f
hoscho <jgeist@gmx.net>
2023-10-23 12:24:25 +0200

    edit SecurityConfig

--- src/main/java/de/jgsoftware/webshop/config/SecurityConfig.java
+++ src/main/java/de/jgsoftware/webshop/config/SecurityConfig.java
@@ -1,17 +1,11 @@
 package de.jgsoftware.webshop.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
